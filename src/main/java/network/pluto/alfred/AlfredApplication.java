725675cb8bdd493fc83fd542dc42d7363f863208
am2rican5 <am2rican5@gmail.com>
2017-09-14 17:12:31 +0900

    config updated

--- src/main/java/network/pluto/alfred/AlfredApplication.java
+++ src/main/java/network/pluto/alfred/AlfredApplication.java
@@ -4,12 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan({"network.pluto.alfred", "network.pluto.bibliotheca"})
+@EnableJpaAuditing
 @EnableJpaRepositories("network.pluto.bibliotheca.repositories")
-@EntityScan("network.pluto.bibliotheca.models")
+@EntityScan(
+        value = "network.pluto.bibliotheca.models",
+        basePackageClasses = Jsr310JpaConverters.class)
 public class AlfredApplication {
 
     public static void main(String[] args) {
